54dfbce3791a90b88b3fb485818e4ea7ece1b6b2
Erick Paiva <123skatesk8@gmail.com>
2022-07-22 22:27:16 -0400

    feat: added new user update routes

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import UserLoginView, UserView
+from .views import UserDetailView, UserLoginView, UserView
 
 urlpatterns = [
     path("accounts/", UserView.as_view()),
+    path("accounts/<pk>/", UserDetailView.as_view()),
+    path("accounts/<pk>/management/", UserDetailView.as_view()),
     path("accounts/newest/<int:num>/", UserView.as_view()),
     path("login/", UserLoginView.as_view()),
 ]
